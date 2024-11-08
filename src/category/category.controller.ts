d4e803576a8b28238585d21d6a0612d698521683
Jeu14 <jedissonsantana34@gmail.com>
2024-11-08 15:20:32 -0300

    feat: Add createCategory validations

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { CategoryService } from './category.service';
-import { CreateCategoryDTO } from './create-category.dto';
+import { CreateCategoryDTO, CreateCategorySchema } from './create-category.dto';
 
 @Controller('categories')
 export class CategoryController {
@@ -9,6 +9,12 @@ export class CategoryController {
     @Post()
     @HttpCode(201)
     async create(@Body() CreateCategoryDTO: CreateCategoryDTO) {
+
+        const { error } = CreateCategorySchema.validate(CreateCategoryDTO);
+        if (error) {
+        throw new BadRequestException(`Validation failed: ${error.message}`);
+        }
+
         const category = await this.categoryService.createCategory(CreateCategoryDTO.names)
 
         return category
--- src/category/create-category.dto.ts
+++ src/category/create-category.dto.ts
@@ -1,4 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger";
+import * as Joi from 'joi';
+
+export const CreateCategorySchema = Joi.object({
+  names: Joi.array().items(Joi.string().required()).min(1).required()
+  .messages({
+    'array.base': 'The "names" field must be an array of strings',
+    'array.min': 'The "names" field must contain at least one category',
+    'array.empty': 'The "names" field cannot be empty',
+    'string.base': 'Each item in "names" must be a string',
+  })
+})
 
 export class CreateCategoryDTO {
 
